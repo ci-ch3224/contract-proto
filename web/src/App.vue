3881d4a52e6223638e4cdda1ccded564c8dc52c2
ch3224bin <ahrocksil@gmail.com>
2021-01-07 14:26:18 +0900

    vuetify 추가

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,28 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <layout-normal></layout-normal>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import LayoutNormal from '@/components/layout/normal.vue'
 
-export default {
-  name: 'App',
+@Component({
   components: {
-    HelloWorld
+    LayoutNormal
   }
+})
+export default class App extends Vue {
+
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
