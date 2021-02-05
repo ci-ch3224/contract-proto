dd99c5a0b85bfa229164f9f345a147c71c82b1b6
ktg5672 <zzang5992@gmail.com>
2021-02-05 10:45:23 +0900

    누락

--- web/src/service/ContractService.ts
+++ web/src/service/ContractService.ts
@@ -1,9 +1,19 @@
 import axios from '@/plugins/axios'
 import { Contract } from '@/model/Contract'
+import { Pageable } from '@/model/Pageable'
 
 class ContractService {
   getAll () {
     return axios.get('/contracts')
+  }
+
+  getPage (pageable: Pageable) {
+    return axios.get('/contracts', {
+      params: pageable,
+      paramsSerializer: (params) => {
+        return this.getQueryString(params)
+      }
+    })
   }
 
   getOne (id: number) {
@@ -12,6 +22,16 @@ class ContractService {
 
   save (data: Contract) {
     return axios.post('/contracts', data)
+  }
+
+  private getQueryString (obj: any): string {
+    let qs = ''
+    Object.entries(obj).forEach(
+      ([key, value]) => {
+        qs += key + '=' + value + '&'
+      }
+    )
+    return qs
   }
 }
 export const contractService = new ContractService()
